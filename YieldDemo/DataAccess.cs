28dda1436cf294b45f7a6408e0ebcaa954cc653b
KraiemKaies <kraiem.kaies@gmail.com>
2022-04-20 14:24:27 +0100

    Final yield return

---
+++ YieldDemo/DataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace YieldDemo
+{
+    public class DataAccess
+    {
+        public static IEnumerable<PersonModel> GetPeople()
+        {
+            //List<PersonModel> output = new List<PersonModel>();
+            //output.Add(new PersonModel("Tim", "Corey"));
+            //output.Add(new PersonModel("Sue", "Storm"));
+            //output.Add(new PersonModel("Jane", "Smith"));
+            //return output;
+
+            yield return new PersonModel("Tim", "Corey");
+            yield return new PersonModel("Sue", "Storm");
+            yield return new PersonModel("Jane", "Smith");
+        }
+    }
+}
--- YieldDemo/Program.cs
+++ YieldDemo/Program.cs
@@ -1,34 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace YieldDemo
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Strat of the App");
-            var people = DataAccess.GetPeople();
-            foreach (var p in people)
+            Console.WriteLine("Start of the App");
+            //var people = DataAccess.GetPeople().Take(2);
+            //foreach (var p in people)
+            //{
+            //    Console.WriteLine($"Read {p.FirstName} {p.LastName}");
+            //}
+            var primeNumbers = Generators.GetPrimeNumbers().Take(5);
+            var primeNumbersList =primeNumbers.ToList();
+            primeNumbersList.Add(140);
+            //var iterator = primeNumbers.GetEnumerator();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (iterator.MoveNext())
+            //    {
+            //        Console.WriteLine(iterator.Current);
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+
+            foreach (var prime in primeNumbersList)
+                
             {
-                Console.WriteLine($"Read { p.FirstName} {p.LastName}");
+                Console.WriteLine(prime);
             }
+
+            //Console.WriteLine("Waiting fot user input");
+            //Console.ReadLine();
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    if (iterator.MoveNext())
+            //    {
+            //        Console.WriteLine(iterator.Current);
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
             Console.WriteLine("End of the App");
         }
     }
-    public class DataAccess
-    {
-        public static IEnumerable<PersonModel> GetPeople()
-        {
-            //List<PersonModel> output = new List<PersonModel>();
-            //output.Add(new PersonModel("Tim", "Corey"));
-            //output.Add(new PersonModel("Sue", "Storm"));
-            //output.Add(new PersonModel("Jane", "Smith"));
-            //return output;
 
-            yield return new PersonModel("Tim", "Corey");
-            yield return new PersonModel("Sue", "Storm");
-            yield return new PersonModel("Jane", "Smith");
+    public class Generators
+    {
+        public static IEnumerable<int> GetPrimeNumbers()
+        {
+            int counter = 1;
+            while (true)
+            {
+                if (IsPrimeNumber(counter)) yield return counter;
+                counter++;
+            }
+        }
+
+        private static bool IsPrimeNumber(int value)
+        {
+            bool output = true;
+            for (int i = 2; i < value/2; i++)
+            {
+                if (value % i == 0)
+                {
+                    output=false;
+                    break;
+                }
+            }
+            return output;
         }
     }
 }
