7f3253bc46d220104c8b2f45b4a0793d840a8f42
KraiemKaies <kraiem.kaies@gmail.com>
2022-04-20 03:22:51 +0100

    yield return

--- YieldDemo/Program.cs
+++ YieldDemo/Program.cs
@@ -20,11 +20,15 @@ namespace YieldDemo
     {
         public static IEnumerable<PersonModel> GetPeople()
         {
-            List<PersonModel> output = new List<PersonModel>();
-            output.Add(new PersonModel("Tim", "Corey"));
-            output.Add(new PersonModel("Sue", "Storm"));
-            output.Add(new PersonModel("Jane", "Smith"));
-            return output;
+            //List<PersonModel> output = new List<PersonModel>();
+            //output.Add(new PersonModel("Tim", "Corey"));
+            //output.Add(new PersonModel("Sue", "Storm"));
+            //output.Add(new PersonModel("Jane", "Smith"));
+            //return output;
+
+            yield return new PersonModel("Tim", "Corey");
+            yield return new PersonModel("Sue", "Storm");
+            yield return new PersonModel("Jane", "Smith");
         }
     }
 }
