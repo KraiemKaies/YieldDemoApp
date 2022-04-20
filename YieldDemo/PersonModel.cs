ca9a25df6f874dbec85ec82ab74611271cb36b45
KraiemKaies <kraiem.kaies@gmail.com>
2022-04-20 03:13:49 +0100

    first commit

---
+++ YieldDemo/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YieldDemo
+{
+    public class PersonModel
+    {
+        public PersonModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Console.WriteLine($"Initialize user { FirstName } { LastName }");
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ YieldDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Strat of the App");
+            var people = DataAccess.GetPeople();
+            foreach (var p in people)
+            {
+                Console.WriteLine($"Read { p.FirstName} {p.LastName}");
+            }
+            Console.WriteLine("End of the App");
+        }
+    }
+    public class DataAccess
+    {
+        public static IEnumerable<PersonModel> GetPeople()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+            output.Add(new PersonModel("Tim", "Corey"));
+            output.Add(new PersonModel("Sue", "Storm"));
+            output.Add(new PersonModel("Jane", "Smith"));
+            return output;
+        }
+    }
+}
